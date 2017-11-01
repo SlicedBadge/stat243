d9d5bdf9f315103e97f141998a5872d8e3f56c64
SlicedBadge <riversj@berkeley.edu>
2017-10-31 17:26:07 -0700

    Finished ps6

--- ps6/s07ex.R
+++ ps6/s07ex.R
@@ -4,19 +4,23 @@ library(stringr)
 library(readr)
 
 #setup parallel
-nCores <- 20
-registerDoParallel(nCores) 
-
-#find files to read in
-files = list.files(path = "/global/scratch/paciorek/wikistats_small/dated/", pattern = "part-",
-                   full.names=TRUE)
-
-result <- foreach(i = files) %dopar% {
-  #read in file to dataframe
-  df = read.delim(i, header = FALSE, sep = " ", quote = "")
-  #get only the entries with Barrack
-  output = df[grep("Barrack_Obama", df$V4),] # this will become part of the out object
+parread <- function(){
+  nCores <- 24
+  registerDoParallel(nCores) 
   
+  #find files to read in
+  files = list.files(path = "/global/scratch/paciorek/wikistats_full/dated_for_R/", pattern = "part-",
+                     full.names=TRUE)
+  
+  result <- foreach(i = files) %dopar% {
+    #read in file to dataframe
+    df = read_delim(i, delim = " ", quote = "")
+    #get only the entries with Barrack
+    output = df[grep("Barrack_Obama", df$V4),] # this will become part of the out object
+    
+  }
+  return(result)
 }
+system.time(result <- parread())
 #combine the results into single dataframe
 df = do.call("rbind", result)
